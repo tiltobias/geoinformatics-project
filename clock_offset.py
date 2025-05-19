c5380723b58a611f23a4dc23aad9fd139bd8bed0
Tobias Andresen <tiltobias@gmail.com>
2025-05-19 15:45:23 +0200

    clock_offset comments

--- clock_offset.py
+++ clock_offset.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-np.random.seed(42)
+np.random.seed(42)  # Seed to reproduce results
 n = 1000
-clock_offset_base = 1000
-clock_offset_change = 0.5
+clock_offset_base = 1000 # 1 microsecond
+clock_offset_change = .5 # 0.5 nanoseconds
 
 # Randomly assign one or zero to n values -> go up or down
 up_or_down = np.random.randint(0, 2, size=n)
@@ -22,15 +23,15 @@ for i in range(1, n): # start at 1 to avoid index error
         clock_offset[i] = clock_offset[i - 1] - clock_offset_change
 
 
+# Write random walk clock offsets to csv 
+df = pd.DataFrame(clock_offset, columns=["Clock Offset"])
+df.to_csv("clock_offset.csv", index=False)
+
 
 # Plot walk
-import matplotlib.pyplot as plt
 plt.plot(clock_offset)
 plt.title("Clock Offset")
 plt.xlabel("Time")
 plt.ylabel("Offset")
 plt.grid()
 plt.show()
-
-df = pd.DataFrame(clock_offset, columns=["Clock Offset"])
-df.to_csv("clock_offset.csv", index=False)
